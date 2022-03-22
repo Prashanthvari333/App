d7e7af53c2118a2bc56f9cc3fca126689e00279e
Prashanthvari333 <prashanthvarikuppala333@gmail.com>
2022-03-22 17:50:18 +0530

    removed extra lines

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
 import React  from 'react';
 import { Text, View, TextInput, Button, ScrollView, Image, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
-import Quiz from './screens/Quiz';
-import Homes from './screens/Homes';
-import Result from './screens/Result';
 import Login from './screens/Login';
 import SingUp from './screens/SingUp';
 
