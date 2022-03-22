79fe9be83ad9804152ed492f044138d0bd765255
Prashanthvari333 <prashanthvarikuppala333@gmail.com>
2022-03-22 17:11:45 +0530

    first upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+import { Text, View, TextInput, Button, ScrollView, Image, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+import Quiz from './screens/Quiz';
+import Homes from './screens/Homes';
+import Result from './screens/Result';
+import Login from './screens/Login';
+import SingUp from './screens/SingUp';
+
+function App() {
+
+    return (
+        <SingUp />
+    );
+}
+export default App;
